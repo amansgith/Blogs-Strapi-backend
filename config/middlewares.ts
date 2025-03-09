9cc13bc9ff39ef6d8d80d443b4fffb0566ebeebb
Aman Khan <amankhancompq2002@gmail.com>
2025-03-09 13:00:00 +0530

    change in cors url

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,8 +7,8 @@ export default [
       contentSecurityPolicy: {
         directives: {
           'script-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https://trustwise-insurance-frontend.onrender.com/', 'https://res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https://trustwise-insurance-frontend.onrender.com/', 'https://res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://trustwise-insurance-frontend.onrender.com', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://trustwise-insurance-frontend.onrender.com', 'https://res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
@@ -17,7 +17,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://trustwise-insurance-frontend.onrender.com/', 'http://localhost:3000'], // Add your Vercel domain here
+      origin: ['https://trustwise-insurance-frontend.onrender.com', 'http://localhost:3000'], // Add your Vercel domain here
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization'],
       keepHeadersOnError: true,
